d09e3139a07bc4a894d9eec29712a83fd6fd6c89
Nell Fauveau <nellousan@hotmail.fr>
2022-11-14 18:42:35 +0900

    faet(discord): added a function to send only once

--- discord.py
+++ discord.py
@@ -4,6 +4,9 @@ import requests
 # This script is used to send a message on a discord channel
 # by using discord webhook. It is configurable by setting up
 # the "discord_webhook" value in conf.json
+
+
+# Sends a notification to the webhook
 def send_discord_message(message: str) -> None:
     f = open("conf.json", "r")
     conf = json.load(f)
@@ -13,6 +16,17 @@ def send_discord_message(message: str) -> None:
     data["username"] = "Asclepius"
     r = requests.post(conf["discord_webhook"], json=data)
     print(r.text)
+
+
+# Sends a notification only once to the webhook
+def send_discord_message_once(message: str) -> None:
+    global notification_sent
+    if notification_sent is False:
+        notification_sent = True
+        send_discord_message(message)
+
+
+notification_sent = False
 
 
 if __name__ == "__main__":
